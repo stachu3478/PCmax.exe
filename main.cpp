be1523b81991c9e328df1852dd9f39e355e2e59f
stachu3478 <stachu3478@o2.pl>
2019-11-11 12:00:21 +0100

    Maintain cmd args handling

--- main.cpp
+++ main.cpp
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-#include <iostream>
-#include <fstream>
-#include <Processor.h>
-#include <chrono>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    auto start = std::chrono::system_clock::now();
-    Processor::solve("m30.txt");
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end-start;
-    std::cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";
-    return 0;
-}
-=======
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
@@ -47,4 +28,3 @@ int main()
 
     return 0;
 }
->>>>>>> fd799c0dfa9f7eaa7280bb74f7011181f2af82ce
