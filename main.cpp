859be21a0d54973967970ad97ba6b9eadf441bf4
stachu3478 <stachu3478@o2.pl>
2019-11-11 11:15:40 +0100

    Unknown commit.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <Processor.h>
+#include <chrono>
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    auto start = std::chrono::system_clock::now();
     Processor::solve("m30.txt");
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";
     return 0;
 }
